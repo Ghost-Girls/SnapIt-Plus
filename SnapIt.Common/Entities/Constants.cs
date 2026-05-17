ac364c6636da3723937069998695b27c729795cd
Ghost-Girls <Jiansong.Jao@Gmail.com>
2026-05-17 17:17:30 +0800

    fix: update AppVersionCheckUrl to Ghost-Girls/SnapIt-Plus repo

--- SnapIt.Common/Entities/Constants.cs
+++ SnapIt.Common/Entities/Constants.cs
@@ -12,7 +12,7 @@ public class Constants
     public static readonly string RootFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), AppName);
     public const string AppFeedbackUrl = "github.com/Ghost-Girls/SnapIt-Plus/issues";
     public const string AppPurchaseUrl = "getsnapit.com/checkout";
-    public const string AppVersionCheckUrl = "raw.githubusercontent.com/enginkirmaci/SnapIt/main/latest-version.json";
+    public const string AppVersionCheckUrl = "raw.githubusercontent.com/Ghost-Girls/SnapIt-Plus/main/latest-version.json";
     public const string AppNewVersionUrl = "github.com/Ghost-Girls/SnapIt-Plus/releases/download/{0}/setup_SnapIt_{0}.exe";
     public const string AppPrivacyUrl = "github.com/Ghost-Girls/SnapIt-Plus/blob/main/PrivacyPolicy.md";
     public const string AppRegistryKey = "SnapIt";
